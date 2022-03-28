8a461ac37b572df892775cfd1619af974a4f7a33
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2022-03-28 06:59:53 +0530

    Complete Project

--- index.js
+++ index.js
@@ -11,3 +11,30 @@ menuIcon.addEventListener('click', () => {
         menuIcon.classList.remove('not-active');
     }
 })
+
+
+
+const productImage = document.querySelector('.product__img')
+const changeImg = (id) => {
+    document.querySelectorAll('.img__holder').forEach(holder => {
+        holder.classList.remove('img__selected')
+    })
+    switch (id) {
+        case 'i01':
+            productImage.src = '/img/swanser-1.png';
+            document.getElementById(id).classList.add('img__selected');
+            break;
+        case 'i02':
+            productImage.src = '/img/swanser-2.png'
+            document.getElementById(id).classList.add('img__selected');
+            break;
+        case 'i03':
+            productImage.src = '/img/swanser-3.png'
+            document.getElementById(id).classList.add('img__selected');
+            break;
+        case 'i04':
+            productImage.src = '/img/swanser-4.png'
+            document.getElementById(id).classList.add('img__selected');
+            break;
+    }
+}
